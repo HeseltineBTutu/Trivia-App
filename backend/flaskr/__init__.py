1fd57cc7018c9b20d80e36b2e7950d33542ec8da
HeseltineBTutu <heseltinetutu@gmail.com>
2022-08-28 11:36:13 -0700

    initial commit

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -8,24 +8,51 @@ from models import setup_db, Question, Category
 
 QUESTIONS_PER_PAGE = 10
 
+def paginate_questions(request, selection):
+    page = request.args.get('page', 1, type=int)
+    start = (page-1) * QUESTIONS_PER_PAGE
+    end = start + QUESTIONS_PER_PAGE
+
+    questions = [question.format() for question in selection]
+    current_questions = questions[start:end]
+
+    return current_questions
+
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
     setup_db(app)
 
-    """
-    @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
-    """
+    #CORS(app)
+    cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
-    """
-    @TODO: Use the after_request decorator to set Access-Control-Allow
-    """
+    #CORS headers 
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Headers', 'Content-Type, Authorization')
+        response.headers.add('Access-Control-Allow-Headers', 'GET, POST, PATCH, DELETE, OPTIONS')
+        return response
+
 
     """
     @TODO:
     Create an endpoint to handle GET requests
     for all available categories.
     """
+    @app.route('/categories')
+    def get_categories():
+        categories = Category.query.order_by(Category.id).all()
+        category = [category.format() for category in categories]
+        
+
+        if len(category)==0:
+            abort(404)
+
+
+        return jsonify({
+            "success":True,
+            "categories":category
+        })
 
 
     """
@@ -40,8 +67,26 @@ def create_app(test_config=None):
     ten questions per page and pagination at the bottom of the screen for three pages.
     Clicking on the page numbers should update the questions.
     """
+    @app.route('/questions', methods=['GET'])
+    def get_questions():
+        selection = Question.query.order_by(Question.id).all()
+        current_questions = paginate_questions(request, selection)
+        current_categories = []
+        categories = Category.query.order_by(Category.type).all()
+
+        if len(current_questions) == 0:
+            abort(404)
+
+        return jsonify({
+            'success':True,
+            'questions': current_questions,
+            'total_questions': len(selection),
+            'current_categories': current_categories,
+            'categories': {category.id : category.type for category in categories}
+        })
 
     """
+    
     @TODO:
     Create an endpoint to DELETE question using a question ID.
 
