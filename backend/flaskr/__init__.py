a292c2b7c166b9d9b8331359c554f41d8efe9c10
HeseltineBTutu <heseltinetutu@gmail.com>
2022-09-12 10:44:40 -0700

    new features

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -1,4 +1,6 @@
+
 import os
+from unicodedata import category
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
@@ -39,19 +41,22 @@ def create_app(test_config=None):
     Create an endpoint to handle GET requests
     for all available categories.
     """
-    @app.route('/categories')
+    @app.route('/categories', methods=['GET'])
     def get_categories():
-        categories = Category.query.order_by(Category.id).all()
-        current_Category = {category.format() for category in categories}
-        
+        categories = Category.query.all()
+        current_category = []
 
+        for category in categories:
+            current_category.append(category.format())
+        
         if len(categories)==0:
             abort(404)
 
 
         return jsonify({
             "success":True,
-            "categories":current_Category
+            "categories":current_category,
+            "total_categories": len(Category.query.all())
         })
 
     @app.route('/questions', methods=['GET'])
@@ -106,27 +111,33 @@ def create_app(test_config=None):
     of the questions list in the "List" tab.
     """
     @app.route('/questions', methods=['POST'])
-    def create_question():
+    def create_question( ):
         body = request.get_json()
+
+        
 
         new_question = body.get('question', None)
         new_answer = body.get('answer', None)
-        new_difficulty = body.get('difficulty', None)
         new_category = body.get('category', None)
-
+        new_difficulty = body.get('difficulty', None)
         try:
-             question = Question(question=new_question, 
-             answer=new_answer, difficulty=new_difficulty, category=new_category )
+        
+            question = Question(question=new_question, 
+            answer=new_answer, category=new_category, difficulty=new_difficulty)
+            print("before", len(Question.query.all()))
 
-             question.insert()
-             selection = Question.query.order_by(Question.id).all()
+            question.insert()
 
-             current_questions = paginate_questions(request, selection)
-             return jsonify({
+            print("after", len(Question.question.all()))
+            selection = Question.query.order_by(Question.id).all()
+
+            current_questions = paginate_questions(request, selection)
+            return jsonify({
                  'success':True,
-                 'created':question.id,
-                 'questions': current_questions
-                 })
+                 'created':Question.id,
+                 'questions': current_questions,
+                 'total_questions': len(Question.query.all())
+                })
         except:
             abort(422)
 
@@ -140,6 +151,28 @@ def create_app(test_config=None):
     only question that include that string within their question.
     Try using the word "title" to start.
     """
+    @app.route('/questions/search', methods=['POST'])
+    def search_questions():
+        body = request.get_json
+        search = body.get('searchTerm', None)
+        try:
+
+            if search == None:
+                abort(404)
+            else:
+                selection = Question.query.filter(Question.question.ilike("%" + search + "%"))
+                questions = []
+
+                for question in selection:
+                    questions.append(question.format())
+
+            return jsonify({
+                'success':True,
+                'questions':questions,
+                'total_questions': len(Question.query.all())
+                })
+        except:
+            abort(404)
 
     """
     @TODO:
@@ -149,6 +182,31 @@ def create_app(test_config=None):
     categories in the left column will cause only questions of that
     category to be shown.
     """
+    @app.route('/categories/<int:category_id>', methods=['GET'])
+    def get_questions_by_category(category_id):
+        category = Category.query.filter(Category.id==category_id).one_or_none()
+        try:
+            
+            if category is None:
+                abort(404)
+            else:
+                selection = Category.query.order_by(Category.id).all()
+                current_questions = paginate_questions(request, selection)
+            if current_questions==0:
+                abort(404)
+
+            return jsonify({
+                'success': True,
+                'questions': current_questions,
+                'total_questions': len(selection),
+                'current_category': category.type
+
+            })
+        except:
+            abort(422)
+
+        
+
 
     """
     @TODO:
@@ -161,12 +219,80 @@ def create_app(test_config=None):
     one question at a time is displayed, the user is allowed to answer
     and shown whether they were correct or not.
     """
+    @app.route('/quizzes', methods=['POST'])
+    def get_question_to_play():
+        body = request.get_json
+        previous_questions = body.get('previous_question', None)
+        category = body.get('quiz_category', None)
+
+        if category['id'] == 0:
+            new_quizzes = Question.query.filter(Question.id.not_in(previous_questions)).all()
+            formatted_quiz = []
+
+            for question in new_quizzes:
+                formatted_quiz.append(question.format())
+        else:
+            new_quizzes = Question.query.filter(Question.id.not_in(previous_questions)
+            ,Question.category==str(['id'])).all()
+
+            forrmated_quiz = []
+
+            for question in new_quizzes:
+
+                forrmated_quiz.append(question.format())
+
+                current_question = None
+                if formatted_quiz:
+                    current_question = random.choice(formatted_quiz)
+
+                return jsonify({
+                    'success':True,
+                    'question':current_question
+                })
+
+
+
+
 
     """
     @TODO:
-    Create error handlers for all expected errors
+    Create error handlers for all expected error
     including 404 and 422.
     """
+    @app.errorhandler(400)
+    def bad_request(error):
+        return({
+            "success": False,
+            "error": 400,
+            "message": "bad request"
+        })
+
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            "success": False,
+            "error":404,
+            "message": "Not found"
+        })
+
+    @app.errorhandler(405)
+    def not_found(error):
+        return({
+            "success":False,
+            "error": 405,
+            "message": "method not allowed"
+        })
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({
+            "success": False,
+            "error":422,
+            "message": "unproccessable"
+        })
+
+
 
     return app
 
